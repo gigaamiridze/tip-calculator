3c00c904f050ee72c2104e4fd373fda9e175c5d2
justtfelix <justttfelix@gmail.com>
2023-02-11 17:35:43 +0400

    Added theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './theme/defaultTheme';
 import Global from './assets/styles/Global';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Global />
-    </>
+    </ThemeProvider>
   )
 }
 
