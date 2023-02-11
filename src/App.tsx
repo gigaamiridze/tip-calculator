b1ad7bfb2cb00475ff5cc38d1bb1ccbe64015b9a
justtfelix <justttfelix@gmail.com>
2023-02-11 18:18:20 +0400

    Added HelmetProvider and fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Helmet } from 'react-helmet';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { defaultTheme } from './theme/defaultTheme';
 import Global from './assets/styles/Global';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Global />
-      <Helmet>
-        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
-      </Helmet>
+      <HelmetProvider>
+        <Global />
+        <Helmet>
+          <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
+        </Helmet>
+      </HelmetProvider>
     </ThemeProvider>
   )
 }
